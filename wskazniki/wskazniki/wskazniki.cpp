c4d4cbe64beee380dc5a40b24c65207a5e1d7768
JanGrzegorski <jan.grzegorski3@gmail.com>
2023-11-21 10:32:12 +0100

    zadanko

---
+++ wskazniki/wskazniki/wskazniki.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int *wsk;
+    int liczba = 2;
+    cout << "Wskazniki int" << endl;
+    wsk = &liczba;
+    cout << "Wsk= " << (int)wsk << endl;
+    cout << "*Wsk= " << *wsk << endl;
+    cout << "Liczba= " << liczba << endl;
+    cout << "&Liczba= " << (int)&liczba << endl;
+    cout << "&Wsk= " << (int)&wsk << endl;
+
+    cout << "------------------------------------------------------------------------------------------------------------------------------------" << endl;
+
+    string *wsk2;
+    string nazwa = "bomba";
+    cout << "Wskazniki string" << endl;
+    wsk2 = &nazwa;
+    cout << "Wsk2= " << (int)wsk2 << endl;
+    cout << "*Wsk2= " << *wsk2 << endl;
+    cout << "nazwa= " << nazwa << endl;
+    cout << "&nazwa= " << (int)&nazwa << endl;
+    cout << "&Wsk2= " << (int)&wsk2 << endl;
+
+    cout << "------------------------------------------------------------------------------------------------------------------------------------" << endl;
+
+    float* wsk3;
+    float liczeb =105.2336;
+    cout << "Wskazniki float" << endl;
+    wsk3 = &liczeb;
+    cout << "Wsk3= " << (int)wsk3 << endl;
+    cout << "*Wsk3= " << *wsk3 << endl;
+    cout << "Liczeb= " << liczeb << endl;
+    cout << "&Liczeb= " << (int)&liczeb << endl;
+    cout << "&Wsk3= " << (int)&wsk3 << endl;
+
+    return 0;
+}
---
+++ zadanie2/zadanie2/zadanie2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	int tablica[5] = { 1, 2, 3, 4, 5 };
+
+	cout << "Tablica = " << tablica << endl;
+	cout << "&Tablica = " << &tablica << endl;
+	cout << "&Tablica[0] = " << &tablica[0] << endl;
+	cout << "&Tablica[1] = " << &tablica[1] << endl;
+	cout << "&Tablica[2] = " << &tablica[2] << endl;
+	cout << "&Tablica[3] = " << &tablica[3] << endl;
+	cout << "&Tablica[4] = " << &tablica[4] << endl << endl;
+
+	cout << "(int)&Tablica[0] = " << (int)&tablica[0] << endl;
+	cout << "(int)&Tablica[1] = " << (int)&tablica[1] << endl;
+	cout << "(int)&Tablica[2] = " << (int)&tablica[2] << endl;
+	cout << "(int)&Tablica[3] = " << (int)&tablica[3] << endl;
+	cout << "(int)&Tablica[4] = " << (int)&tablica[4] << endl;
+
+	int* wsk;
+	wsk = &tablica[0];
+	cout << endl << endl;
+	cout << "Przeniesienie o 2 x 4 bajty = " << (int)(wsk += 2) << endl;
+	cout << "Przeniesienie o kolejne 2 bajty = " << (int)(wsk += 2) << endl;
+	cout << "Przeniesienie o kolejne 2 wyjdziemy poza adresy el. tablicy = " << (int)(wsk += 2) << endl;
+
+	//do domu na string
+
+	return 0;
+}
